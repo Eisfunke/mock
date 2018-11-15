9b54327c31a40a9679d3730ca6809e63fd496b8e
Nicolas Lenz <nicolas@eisfunke.com>
2018-11-15 23:30:35 +0100

    Init Mock.hs

---
+++ Mock.hs
@@ -0,0 +1,20 @@
+module Mock (mockAlternate, mockRandom) where
+
+import Data.Char
+import Data.List
+import System.Random
+import Data.Time.Clock.POSIX
+
+toUpperBy :: String -> [Bool] -> String
+toUpperBy (c:cs) (True:bs) = toUpper c : toUpperBy cs bs
+toUpperBy (c:cs) (False:bs) = c : toUpperBy cs bs
+toUpperBy (cs) [] = cs
+toUpperBy [] _ = []
+
+mockAlternate :: String -> String
+mockAlternate str = toUpperBy str $ intersperse True $ repeat False
+
+mockRandom :: String -> IO String
+mockRandom str = do
+    time <- fmap round getPOSIXTime
+    return $ toUpperBy str $ randoms $ mkStdGen time
